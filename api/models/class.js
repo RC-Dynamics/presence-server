965c8fe65b0bda2b196b816d944471ba2db77f64
Roberto Fernandes <rcf6@cin.ufpe.br>
2017-06-12 16:29:04 -0300

    Adding a field in Class Schema

--- api/models/class.js
+++ api/models/class.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ClassSchema = new Schema({
-	teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', default: {} },
+	teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', default: {}, required: true },
   students: [{ type: Schema.Types.ObjectId, ref: 'Teacher', default: {}}],
-  startTime: { type: Date, default: Date.now}
+  startTime: { type: Date, default: Date.now, required: true },
+	endTime: { type: Date, default: Date.now, required: true },
+	id: {type: Number, required: true }
 });
 
 module.exports = mongoose.model('Class', ClassSchema);
