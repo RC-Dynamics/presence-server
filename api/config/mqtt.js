52b8df3cff0b19c15ba71ebf7be4c3245485d9f1
Roberto Fernandes <rcf6@cin.ufpe.br>
2017-06-12 15:39:04 -0300

    Starting to develop the mqtt communication

--- api/config/mqtt.js
+++ api/config/mqtt.js
@@ -1,5 +1,18 @@
 const mqtt = require('mqtt');
+var student_controller = require('./api/controllers/student.controller');
+var teacher_controller = require('./api/controllers/teacher.controller');
+var class_controller = require('./api/controllers/class.controller');
 
 module.exports = () => {
-  
+  const client = mqtt.connect('') // NEED TO DEFINE THE URL
+
+  client.on('connect', () => {
+    client.subscribe('') // NEED TO DEFINE THE TOPIC'S NAME == NEW READ
+  })
+
+  client.on('message', (topic, message) => {
+    if(topic === '') { // NEW READ
+
+    }
+  })
 }
--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 
 var meshblu = require('./api/config/meshblu');
 var config = require('./api/config/config');
-// var mongoose = require('./api/config/mongoose');
+var mongoose = require('./api/config/mongoose');
 var mqtt = require('./api/config/mqtt');
 
 // Initializing the data base
 var db = mongoose();
+
 // Initializing the communication with the Knot Cloud
 var mesh = meshblu();
+
+// Initializing the mqtt communication
+// var comm = mqtt();
 
 // Adding a new student
 // var controller = require('./api/controllers/student.controller');
