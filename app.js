bb3d87889ea973904da7f220a327d38fe86f2719
Roberto Fernandes <rcf6@cin.ufpe.br>
2017-06-12 15:31:43 -0300

    Adding mqtt opition

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 
 var meshblu = require('./api/config/meshblu');
 var config = require('./api/config/config');
-var mongoose = require('./api/config/mongoose');
+// var mongoose = require('./api/config/mongoose');
+var mqtt = require('./api/config/mqtt');
 
 // Initializing the data base
 var db = mongoose();
 // Initializing the communication with the Knot Cloud
 var mesh = meshblu();
+
+// Adding a new student
+// var controller = require('./api/controllers/student.controller');
+// controller.addStudent('ACris', '43313', '134311');
