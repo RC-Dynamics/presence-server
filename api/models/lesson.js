281cd58cf9038c6ae8582806f329253921175e27
Roberto Fernandes <rcf6@cin.ufpe.br>
2017-06-12 21:34:49 -0300

    Removing default value in schema

--- api/models/lesson.js
+++ api/models/lesson.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var LessonSchema = new Schema({
-	teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', default: {}, required: true },
-  students: [{ type: Schema.Types.ObjectId, ref: 'Student', default: {}}],
+	teacher: { type: Schema.Types.ObjectId, ref: 'Teacher', required: true },
+  students: [{ type: Schema.Types.ObjectId, ref: 'Student'}],
   startTime: { type: Date, default: Date.now(), required: true },
 	endTime: { type: Date, default: Date.now(), required: true },
 	idThing: {type: Number, required: true }
