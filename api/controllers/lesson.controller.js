693b609cd20be1bade7ffe2ce9c653d867ee6b1e
Roberto Fernandes <rcf6@cin.ufpe.br>
2017-06-12 21:38:33 -0300

    Fixing bugs with the objectId

--- api/controllers/lesson.controller.js
+++ api/controllers/lesson.controller.js
@@ -2,7 +2,7 @@ var Lesson = require('mongoose').model('Lesson');
 
 var createLesson = (info) => {
   var lesson = new Lesson ();
-  lesson.teacher = info.teacher;
+  lesson.teacher = info.teacher._id;
   lesson.startTime = info.startTime;
   lesson.idThing = info.idThing;
   lesson.endTime = info.startTime + (1000*60*60*3);
@@ -12,12 +12,13 @@ var createLesson = (info) => {
 }
 
 var addStudent = (_info) => {
-  Lesson.find({id: _info.idThing})
+  Lesson.find({idThing: _info.idThing})
   .where('startTime').lte(_info.currTime)
-  .where('endTime').gte(_info.currTime)
-  .exec((err, lesson) => {
-    lesson.students.push(_info.student);
-  });
+  .where('endTime').gte(_info.currTime), (err, lesson) => {
+    if(err) throw err;
+    console.log(lesson[0]);
+    lesson[0].students.push(_info.student._id);
+  };
 }
 
 module.exports = {
