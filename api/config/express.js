2a0f7017398b8c0d4cce998396628935532aecf9
Roberto Fernandes <rcf6@cin.ufpe.br>
2017-06-13 21:32:20 -0300

    Adding express dependency

---
+++ api/config/express.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+module.exports = () => {
+  var app = express();
+
+  return app;
+}
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-var meshblu = require('./api/config/meshblu');
-var config = require('./api/config/config');
-var mongoose = require('./api/config/mongoose');
-var mqtt = require('./api/config/mqtt');
+const meshblu = require('./api/config/meshblu');
+const config = require('./api/config/config');
+const mongoose = require('./api/config/mongoose');
+const mqtt = require('./api/config/mqtt');
+const express = require('./api/config/express');
 
 // Initializing the data base
 var db = mongoose();
 
 // Initializing the communication with the Knot Cloud
 var mesh = meshblu();
+
+// Initializing the express app
+var app = express();
+app.listen(config.port);
 
 // Initializing the mqtt communication
 // var comm = mqtt();
